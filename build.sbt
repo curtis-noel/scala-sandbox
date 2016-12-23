720918ab0c876cc2672893ff24a60aed8f22e927
curtisnoel <curtis.noel@gmail.com>
2016-12-22 20:39:47 -0500

    Initial commit

---
+++ build.sbt
@@ -0,0 +1,8 @@
+name := "scala-sandbox"
+
+version := "1.0"
+
+scalaVersion := "2.12.1"
+
+libraryDependencies += "org.apache.spark" %% "spark-core" % "2.0.2"
+    
---
+++ src/main/scala/Demo.scala
@@ -0,0 +1,15 @@
+// examples from Learn Scala Programming Language.
+//Singh, Virender (2016-05-21). Learn Scala Programming Language. (Kindle Location 2). Unknown. Kindle Edition.
+
+object Demo {
+  def main( args: Array[ String]) {
+    var myVar :Int = 10;
+    val myVal :String = "Hello Scala with datatype declaration.";
+    var myVar1 = 20;
+    val myVal1 = "Hello Scala new without datatype declaration.";
+    println( myVar);
+    println( myVal);
+    println( myVar1);
+    println( myVal1);
+  }
+}
