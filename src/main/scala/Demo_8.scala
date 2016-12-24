04bab1d5f38d5b3b693fa8b3e0eeeb377860a385
curtisnoel <curtis.noel@gmail.com>
2016-12-24 12:32:21 -0500

    added precedence

--- src/main/scala/Demo_8.scala
+++ src/main/scala/Demo_8.scala
@@ -3,7 +3,25 @@
   *
   * bitwise ops
   *
-  */
+  * -----------------------------------------------------------------------
+  * Precedence
+  * -----------------------------------------------------------------------
+  * Postfix () [] Left to right
+  * Unary ! ~ Right to left
+  * Multiplicative * / % Left to right
+  * Additive + Left to right
+  * Shift > > > > > < < Left to right
+  * Relational > > = < < = Left to right
+  * Equality = = != Left to right
+  * Bitwise AND & Left to right
+  * Bitwise XOR ^ Left to right
+  * Bitwise OR | Left to right
+  * Logical AND && Left to right
+  * Logical OR | | Left to right
+  * Assignment = + = -= *= /= %= > > = < < = &= ^ = | = Right to left
+  * Comma , Left to right
+  * -----------------------------------------------------------------------
+
 object Demo_8 {
 
   def toBinary(i: Int, digits: Int = 8) =
